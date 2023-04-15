d363c3523c04fd0f29845289fc7931868cef43f5
GonzaloEspinoza <128237859+KingDmnT@users.noreply.github.com>
2023-04-15 12:29:39 -0400

    modificacion js validadores

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 var rut = document.getElementById("rut");
- var password = document.getElementById("password");
- var error = document.getElementById("error");
- error.style.color = "red";
+var appaterno = document.getElementById("rut");
+var apmaterno = document.getElementById("rut");
+var nombre = document.getElementById("rut");
+var fechanacimiento = document.getElementById("rut");
+var edad = document.getElementById("rut");
+var genero = document.getElementById("rut");
+var email = document.getElementById("rut");
+var celular = document.getElementById("rut");
+var profesion = document.getElementById("rut");
+var motivacion = document.getElementById("rut");
+var error = document.getElementById("error");
+error.style.color = "red";
 
 //function enviarFormulario(){
   //  console.log("Enviando Formulario ...");
@@ -27,12 +36,16 @@ var form = document.getElementById("formulario");
         evt.preventDefault();
         var mensajesError = [];
 
-    if (nombre.value === null || nombre.value === ""){
-        mensajesError.push("Ingresa tu nombre");
+    if (rut.value === null || rut.value === ""){
+        mensajesError.push("Ingresa tu rut");
     }
 
-    if (password.value === null || password.value === ""){
-        mensajesError.push("Ingresa tu password");
+    if (appaterno.value === null || appaterno.value === ""){
+        mensajesError.push("Ingresa tu apellido paterno");
+    }
+
+    if (apmaterno.value === null || apmaterno.value === ""){
+        mensajesError.push("Ingresa tu apellido materno");
     }
 
     error.innerHTML = mensajesError.join(", ")
