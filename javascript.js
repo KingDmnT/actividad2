0c9f1903873cb23631c7d69ab6d10dd295545217
KingDmnT <gonz.espinoza@duocuc.cl>
2023-04-15 23:43:29 -0400

    añadir validadores js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ var apmaterno = document.getElementById("rut");
 var nombre = document.getElementById("rut");
 var fechanacimiento = document.getElementById("rut");
 var edad = document.getElementById("rut");
-var genero = document.getElementById("rut");
+var generos = document.getElementById("rut");
 var email = document.getElementById("rut");
 var celular = document.getElementById("rut");
 var profesion = document.getElementById("rut");
@@ -46,6 +46,30 @@ var form = document.getElementById("formulario");
 
     if (apmaterno.value === null || apmaterno.value === ""){
         mensajesError.push("Ingresa tu apellido materno");
+    }
+    if (nombre.value === null || nombre.value === ""){
+        mensajesError.push("Ingresa tu nombre");
+    }
+    if (fechanacimiento.value === null || fechanacimiento.value === ""){
+        mensajesError.push("Ingresa tu fecha de nacimiento");
+    }
+    if (edad.value === null || edad.value === ""){
+        mensajesError.push("Ingresa tu edad");
+    }
+    if (generos.value === null || generos.value === ""){
+        mensajesError.push("Ingresa tu genero");
+    }
+    if (email.value === null || email.value === ""){
+        mensajesError.push("Ingresa tu email");
+    }
+    if (celular.value === null || celular.value === ""){
+        mensajesError.push("Ingresa tu celular");
+    }
+    if (profesion.value === null || profesion.value === ""){
+        mensajesError.push("Ingresa tu profesion");
+    }
+    if (motivacion.value === null || motivacion.value === ""){
+        mensajesError.push("Ingresa tu motivacion en el campo requerido");
     }
 
     error.innerHTML = mensajesError.join(", ")
